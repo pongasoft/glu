443174d6a1af8a51e83e00b36577b0066f4e5aaf
Yan Pujante <yan@pongasoft.com>
2011-03-01 10:29:36 -0800

    enhanced copyright wording

--- console/org.linkedin.glu.console-server/src/cmdline/resources/bin/consolectl.sh
+++ console/org.linkedin.glu.console-server/src/cmdline/resources/bin/consolectl.sh
@@ -2,7 +2,7 @@
 
 #
 # Copyright (c) 2010-2010 LinkedIn, Inc
-# Copyright (c) 2011 Yan Pujante
+# Portions Copyright (c) 2011 Yan Pujante
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not
 # use this file except in compliance with the License. You may obtain a copy of
