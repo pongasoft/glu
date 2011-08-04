6477f728bd88b88cc3001b1ff501fe5081aa486d
Ran Tavory <rantav@gmail.com>
2011-08-04 12:25:54 +0300

    Add a hard-coded version display of the static model. still needs work, but basic graphing is in place

--- console/org.linkedin.glu.console-webapp/grails-app/controllers/org/linkedin/glu/console/controllers/GraphController.groovy
+++ console/org.linkedin.glu.console-webapp/grails-app/controllers/org/linkedin/glu/console/controllers/GraphController.groovy
@@ -16,22 +16,114 @@
 
 package org.linkedin.glu.console.controllers
 
+import org.linkedin.glu.console.domain.DbSystemModel
 import org.linkedin.glu.console.domain.User
 import org.apache.shiro.SecurityUtils
 import org.linkedin.glu.grails.utils.ConsoleConfig
+import org.linkedin.glu.provisioner.core.model.SystemModel;
 
 /**
  * Graph controller
  *
  * @author rantav@gmail.com 
  **/
-class GraphController extends ControllerBase
-{
+class GraphController extends ControllerBase {
+
   ConsoleConfig consoleConfig
 
   def index = {
   }
 
   def graph = {
+    DbSystemModel dbmodel = DbSystemModel.findCurrent(request.fabric)
+    SystemModel model = dbmodel.systemModel
+    List<MaxMinVersion> versions = extractMaxMinVersions(model)
+    [versions: versions]
+  }
+
+  private def extractMaxMinVersions(SystemModel model) {
+    Map<String, MaxMinVersion> moduleVersions = new HashMap<String, MaxMinVersion>();
+    model.findEntries().each { entry ->
+      String module = entry.metadata.get("product")
+      int version = entry.metadata.get("version")
+      if (version > 0) {
+        String agent = entry.getAgent()
+        MaxMinVersion maxMin = moduleVersions.get(module)
+        if (maxMin == null) {
+          Set<String> agents = new HashSet<String>()
+          agents.add(agent)
+          maxMin = new MaxMinVersion(module, version, version, agents, agents)
+          moduleVersions.put(module, maxMin)
+        }
+        maxMin.addVersion(version, agent)
+      }
+    }
+    List<MaxMinVersion> ret = new ArrayList<MaxMinVersion>(moduleVersions.values());
+    return ret;
+  }
+
+  class MaxMinVersion {
+    int min, max
+    String module
+    private Set<String> minAgents = new HashSet<String>()
+    private Set<String> maxAgents = new HashSet<String>()
+
+    public MaxMinVersion(String module, int min, int max, Set<String> minAgents, Set<String> maxAgents) {
+      this.module = module;
+      this.min = min;
+      this.max = max;
+      this.minAgents = new HashSet<String>(minAgents);
+      this.maxAgents = new HashSet<String>(maxAgents);
+    }
+    
+    /**
+     * Adding a version means that the object checks if this version is either higher than the max or
+     * lower than the min and if it is, updates max/min accordingly and sets the agents set to the
+     * given agent.
+     * If the version equals max/min then the agent is added to the set of already existing agents.
+     * @param version
+     * @param agent
+     */
+    def addVersion(int version, String agent) {
+      if (version == min) {
+        minAgents.add(agent);
+      } else if (version < min) {
+        min = version;
+        minAgents.clear();
+        minAgents.add(agent);
+      }
+
+      if (version == max) {
+        maxAgents.add(agent);
+      } else if (version > max) {
+        max = version;
+        maxAgents.clear();
+        maxAgents.add(agent);
+      }
+    }
+    
+    public String getMinAgentsHtml() {
+      agentsToHtml(minAgents);
+    }
+
+    public String getMaxAgentsHtml() {
+      if (min == max) {
+        return "SAME";
+      }
+      agentsToHtml(maxAgents);
+    }
+    
+    private String agentsToHtml(Set<String> agents) {
+      StringBuffer sb = new StringBuffer();
+      for (String agent : agents) {
+        // http://glu/console/agents/view/anent-name
+        sb.append(String.format("<a href=\"/console/agents/view/%s\">%s</a><br/>", agent, agent));
+      }
+      return sb.toString();
+    }
+    
+    String toString () {
+      "${module} ${min}-${max}"
+    }
   }
 }
--- console/org.linkedin.glu.console-webapp/grails-app/views/graph/graph.gsp
+++ console/org.linkedin.glu.console-webapp/grails-app/views/graph/graph.gsp
@@ -1,26 +1,56 @@
-%{--
-  - Copyright (c) 2010-2010 LinkedIn, Inc
-  -
-  - Licensed under the Apache License, Version 2.0 (the "License"); you may not
-  - use this file except in compliance with the License. You may obtain a copy of
-  - the License at
-  -
-  - http://www.apache.org/licenses/LICENSE-2.0
-  -
-  - Unless required by applicable law or agreed to in writing, software
-  - distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-  - WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-  - License for the specific language governing permissions and limitations under
-  - the License.
-  --}%
+%{-- - Copyright (c) 2010-2010 LinkedIn, Inc - - Licensed under the
+Apache License, Version 2.0 (the "License"); you may not - use this file
+except in compliance with the License. You may obtain a copy of - the
+License at - - http://www.apache.org/licenses/LICENSE-2.0 - - Unless
+required by applicable law or agreed to in writing, software -
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT - WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+implied. See the - License for the specific language governing
+permissions and limitations under - the License. --}%
 
 <html>
 <head>
-  <title>GLU Console - ${user.username.encodeAsHTML()}</title>
-  <meta name="layout" content="main"/>
+<title>GLU Console - ${user.username.encodeAsHTML()}
+</title>
+<meta name="layout" content="main" />
 </head>
 <body>
-<h2>TODO: Display A single graph here</h2>
-<img src="http://www.jpowered.com/php-scripts/area-graph/images/area-graph-311x331.gif"/>
+	<h2>Graph: Versions</h2>
+	<div id='chart' style='width: 100%; height: 700px;'></div>
+	<div id='table' style='margin-top: 20px'></div>
+	<script type="text/javascript" src="http://www.google.com/jsapi"></script>
+	<script type="text/javascript">
+   google.load('visualization', '1', {packages: ['corechart', 'table']});
+  </script>
+	<script type="text/javascript">
+function drawVisualization() {
+  // Populate the data table.
+  var data = google.visualization.arrayToDataTable([
+<g:each in="${versions}" var="entry">
+     ['${entry.module}',${entry.min}, ${entry.max}, '${entry.minAgentsHtml}', '${entry.maxAgentsHtml}'],
+</g:each>
+   ], true);
+  data.setColumnLabel(0, 'Module');
+  data.setColumnLabel(1, 'Min');
+  data.setColumnLabel(2, 'Max');
+  data.setColumnLabel(3, 'Min Agents');
+  data.setColumnLabel(4, 'Max Agents');
+
+  // Draw the chart.
+  var chart = new google.visualization.CandlestickChart(document.getElementById('chart'));
+  var chartView = new google.visualization.DataView(data);
+  chartView.setColumns([0, 1, 1, 2, 2]);
+  chart.draw(chartView, {legend:'none', hAxis: {showTextEvery: 1, slantedText: true, slantedTextAngle: 90},
+    title: 'Versions variation according to the STATIC model',
+    	vAxis: {},
+    	chartArea: {width: "80%"}});
+
+  var table = new google.visualization.Table(document.getElementById('table'));
+  var tableView = new google.visualization.DataView(data);
+  tableView.setColumns([0, 1, 3, 2, 4]);
+  table.draw(tableView, {showRowNumber: false, allowHtml: true});
+}
+google.setOnLoadCallback(drawVisualization);
+ </script>
 </body>
 </html>
