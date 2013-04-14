40e287117773163a55b46fa223df2e367bf8c676
Yan Pujante <yan@pongasoft.com>
2013-04-14 11:27:35 -1000

    #205: exposed zookeeperRoot

--- console/org.linkedin.glu.console-webapp/grails-app/conf/Config.groovy
+++ console/org.linkedin.glu.console-webapp/grails-app/conf/Config.groovy
@@ -39,6 +39,8 @@ console.deltaService.stateDeltaOverridesDelta = true
 // set to false if you want missing agents to not be skipped anymore in plan computation
 console.plannerService.planner.skipMissingAgents = true
 
+console.trackerService.zookeeperRoot = '/org/glu'
+
 // connection timeout when the console tries to talk to the agent (rest)
 console.to.agent.connectionTimeout = "30s"
 
@@ -431,7 +433,7 @@ environments {
     plugins.StreamFileContentPlugin.unrestrictedLocation = '/export/content/glu'
     plugins.StreamFileContentPlugin.maskFileContent = true
 
-    grails.serverURL = "http://localhost:8080/${appName}"
+    grails.serverURL = "http://${InetAddress.getLocalHost().canonicalHostName}:8080/${appName}"
 
     // define the plugins as a Map, or a class name or an array of class names
     orchestration.engine.plugins = [
