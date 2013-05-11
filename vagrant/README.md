51aa01794ebaaae668a8a49332b86bafd4a2880f
Andrew Price <andrew.price@ensighten.com>
2013-05-11 01:39:20 -0700

    -allow specifying glu version to install as a cli argument handled by the vagrantfile -install apache on the vm and proxy to console webapp; this removes need to update hosts file on host os -added apache config files -updated readme

--- vagrant/README.md
+++ vagrant/README.md
@@ -8,15 +8,16 @@ Steps (for OSX host)
 
 1. Install newest [Virtual Box](https://www.virtualbox.org/)
 1. Install newest [Vagrant](http://downloads.vagrantup.com/)
-1. `sudo sh -c 'echo "127.0.0.1 	glu-tutorial" >> /private/etc/hosts'`
-1. `sudo dscacheutil -flushcache`
-1. `git clone git@github.com:Ensighten/glu-vagrant.git`
-1. `cd glu-vagrant`
+1. `git clone git@github.com:pongasoft/glu.git`
+1. `cd glu/vagrant`
 1. `vagrant up`
 1. `vagrant ssh`
 1. `sudo su -`
 1. `cd /var/lib/glu`
 1. Now you are ready to follow along with the [glu tutorial](http://pongasoft.github.io/glu/docs/latest/html/tutorial.html)
+
+NOTE: If you see errors from the glu tutorial script about JAVA_HOME not being set, run the command as follows: 
+`JAVA_HOME=/usr/lib/jvm/java-7-oracle JRE_HOME=/usr/lib/jvm/java-7-oracle/jre ./bin/tutorial.sh`
 
 Forwarded Ports
 ---------------
@@ -24,9 +25,12 @@ The following ports on the VM are forwarded to the host:
 
 | Application | Guest Port | Host Port |
 |---|---|---|
-| console | 8080 | 8080 |
+| apache proxy to glu console | 8000 | 8000 |
+| glu console | 8080 | 8080 |
 | webapp1 | 9000 | 9000 |
 | webapp2 | 9001 | 9001 |
 | webapp3 | 9002 | 9002 |
 
-* [console](http://localhost:8080/console)
+* [console](http://localhost:8000/console)
+
+NOTE: The apache frontend to glu rewrites the location header to deal with the issue mentioned [here](http://glu.977617.n3.nabble.com/Glu-console-does-more-absolute-url-redirects-with-4-7-0-td4025588.html).
--- vagrant/install-glu.sh
+++ vagrant/install-glu.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
+set -xv
+
 if [ -e /var/lib/glu ]
 then
 	echo "glu already installed"
 	exit 0
+fi
+
+GLU_VERSION=$1
+if [ -z "$GLU_VERSION" ]
+then
+	echo "ERROR: GLU_VERSION not specified"
+	exit 1
 fi
 
 # run all apt commands in noninteractive mode
@@ -16,14 +25,27 @@ wget -q https://github.com/flexiondotorg/oab-java6/raw/0.2.7/oab-java.sh -O oab-
 chmod +x oab-java.sh
 ./oab-java.sh -7
 apt-get -q -y -f install oracle-java7-jre oracle-java7-bin oracle-java7-jdk
-echo 'JAVA_HOME=/usr/lib/jvm/java-7-oracle' >> /etc/environment
-echo 'JRE_HOME=/usr/lib/jvm/java-7-oracle/jre' >> /etc/environment
-. /etc/environment
+export JAVA_HOME=/usr/lib/jvm/java-7-oracle
+export JRE_HOME=/usr/lib/jvm/java-7-oracle/jre
+echo 'export JAVA_HOME=/usr/lib/jvm/java-7-oracle' >> /etc/profile
+echo 'export JRE_HOME=/usr/lib/jvm/java-7-oracle/jre' >> /etc/profile
+echo 'export JAVA_HOME=/usr/lib/jvm/java-7-oracle' >> /etc/bash.bashrc
+echo 'export JRE_HOME=/usr/lib/jvm/java-7-oracle/jre' >> /etc/bash.bashrc
 ln -s $JRE_HOME/bin/java /bin/java
+
+# install and configure apache
+apt-get -y install apache2
+a2enmod proxy_http
+a2enmod headers
+cp /vagrant/default /etc/apache2/sites-available/default
+cp /vagrant/ports.conf /etc/apache2/ports.conf
+service apache2 restart
 
 # install glu tutorial
 GLU_TUTORIAL_ROOT=/var/lib/glu
 mkdir -p $GLU_TUTORIAL_ROOT
 cd $GLU_TUTORIAL_ROOT
-wget -q http://dl.bintray.com/content/pongasoft/glu/org.linkedin.glu.packaging-all-5.0.0.tgz?direct -O glu.tgz
+wget -q http://dl.bintray.com/content/pongasoft/glu/org.linkedin.glu.packaging-all-${GLU_VERSION}.tgz?direct -O glu.tgz
 tar zxf glu.tgz -C $GLU_TUTORIAL_ROOT --strip-components=1
+#JAVA_HOME=/usr/lib/jvm/java-7-oracle JRE_HOME=/usr/lib/jvm/java-7-oracle/jre ./bin/tutorial.sh setup
+#JAVA_HOME=/usr/lib/jvm/java-7-oracle JRE_HOME=/usr/lib/jvm/java-7-oracle/jre ./bin/tutorial.sh start
