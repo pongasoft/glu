aa515b9534a000a0cd384349bfa26da5b930d3c2
Yan Pujante <yan@pongasoft.com>
2013-06-03 10:23:50 -1000

    #58: added test for more than 1 server

--- packaging/org.pongasoft.glu.packaging-setup-impl/src/test/groovy/test/setup/TestZooKeeperPackager.groovy
+++ packaging/org.pongasoft.glu.packaging-setup-impl/src/test/groovy/test/setup/TestZooKeeperPackager.groovy
@@ -10,12 +10,14 @@ import org.pongasoft.glu.packaging.setup.ZooKeeperPackager
  * @author yan@pongasoft.com  */
 public class TestZooKeeperPackager extends GroovyTestCase
 {
+  public static Object DIRECTORY = new Object()
+
+  /**
+   * A single package */
   public void testCreatePackage1()
   {
     ShellImpl.createTempShell { Shell shell ->
       def dist = shell.mkdirs('/dist')
-
-
 
       // building a package with a readme at the root and a another file in a subdirectory..
       // we will make sure that the packaging process does not affect anything else
@@ -35,33 +37,77 @@ public class TestZooKeeperPackager extends GroovyTestCase
       assertEquals(1, zkps.size())
       Resource zkp = zkps.iterator().next()
 
-      Object directory = new Object()
+      assertEquals("/out/org.linkedin.zookeeper-server-x.y.z-h2-2181", zkp.path)
+
+      def expectedResources =
+        [
+          '/README.md': 'this is the readme',
+          '/lib': DIRECTORY,
+          '/lib/acme.jar': 'this is the jar',
+          '/conf': DIRECTORY,
+          '/conf/zoo.cfg': '[clientPort:2181];[host:h2];[]'
+        ]
+
+      checkPackageContent(expectedResources, zkp)
+    }
+  }
+
+  public void testCreatePackage3()
+  {
+    ShellImpl.createTempShell { Shell shell ->
+      def dist = shell.mkdirs('/dist')
+
+      // building a package with a readme at the root and a another file in a subdirectory..
+      // we will make sure that the packaging process does not affect anything else
+      def zkDistPackage = dist.createRelative('org.linkedin.zookeeper-server-x.y.z')
+      shell.saveContent(zkDistPackage.createRelative('README.md'), "this is the readme")
+      shell.saveContent(zkDistPackage.createRelative('lib/acme.jar'), "this is the jar")
+
+
+      def packager = new ZooKeeperPackager(shell: shell,
+                                           templatesRoot: createZooKeeperTemplates(shell),
+                                           zookeperServers: [[host: 'h2'], [clientPort: 2183, quorumPort: 2889, leaderElectionPort: 3889]],
+                                           inputPackage: zkDistPackage,
+                                           outputFolder: shell.mkdirs('/out'))
+
+      List<Resource> zkps = packager.createPackages() as List
+      assertEquals(2, zkps.size())
+
+      // server 1 -> h2-2181
+      Resource zkp = zkps[0]
 
       assertEquals("/out/org.linkedin.zookeeper-server-x.y.z-h2-2181", zkp.path)
 
       def expectedResources =
         [
           '/README.md': 'this is the readme',
-          '/lib': directory,
+          '/data': DIRECTORY,
+          '/data/myid': '1',
+          '/lib': DIRECTORY,
           '/lib/acme.jar': 'this is the jar',
-          '/conf': directory,
-          '/conf/zoo.cfg': '[clientPort:2181];[host:h2];[]'
+          '/conf': DIRECTORY,
+          '/conf/zoo.cfg': '[clientPort:2181];[host:h2];[[host:h2], [clientPort:2183, quorumPort:2889, leaderElectionPort:3889]]'
         ]
 
-      GroovyIOUtils.eachChildRecurse(zkp.chroot('.')) { Resource r ->
-        println r.path
+      checkPackageContent(expectedResources, zkp)
 
-        def expectedValue = expectedResources.remove(r.path)
-        if(expectedValue == null)
-          fail("unexpected resource ${r}")
+      // server 2 -> localhost-2183
+      zkp = zkps[1]
 
-        if(expectedValue.is(directory))
-          assertTrue("${r} is directory", r.isDirectory())
-        else
-          assertEquals(expectedValue, r.file.text)
-      }
+      assertEquals("/out/org.linkedin.zookeeper-server-x.y.z-localhost-2183", zkp.path)
 
-      assertTrue("${expectedResources} is not empty", expectedResources.isEmpty())
+      expectedResources =
+        [
+          '/README.md': 'this is the readme',
+          '/data': DIRECTORY,
+          '/data/myid': '2',
+          '/lib': DIRECTORY,
+          '/lib/acme.jar': 'this is the jar',
+          '/conf': DIRECTORY,
+          '/conf/zoo.cfg': '[clientPort:2181];[clientPort:2183, quorumPort:2889, leaderElectionPort:3889];[[host:h2], [clientPort:2183, quorumPort:2889, leaderElectionPort:3889]]'
+        ]
+
+      checkPackageContent(expectedResources, zkp)
     }
   }
 
@@ -75,5 +121,21 @@ public class TestZooKeeperPackager extends GroovyTestCase
     shell.saveContent(templates.createRelative("/zoo.cfg.gtmpl"), '${opts};${zk};${allServers}')
 
     return templates
+  }
+
+  private void checkPackageContent(def expectedResources, Resource pkgRoot)
+  {
+    GroovyIOUtils.eachChildRecurse(pkgRoot.chroot('.')) { Resource r ->
+      def expectedValue = expectedResources.remove(r.path)
+      if(expectedValue == null)
+        fail("unexpected resource ${r}")
+
+      if(expectedValue.is(DIRECTORY))
+        assertTrue("${r} is directory", r.isDirectory())
+      else
+        assertEquals(expectedValue, r.file.text)
+    }
+
+    assertTrue("${expectedResources} is not empty", expectedResources.isEmpty())
   }
 }
